d3243960729ba856964ae20f651db9a5fb13fbba
yamaki koudai <s23029@std.it-college.ac.jp>
2023-07-06 07:22:49 +0000

    2.7 リスト

---
+++ CHAPTER02/Q2_7_1.py
@@ -0,0 +1,6 @@
+rist = []
+rist1 = rist.append("oeange")
+rist2 = rist.append("apple")
+rist3 = rist.append("grape")
+rist4 = rist.append("banana")
+print(rist)
---
+++ CHAPTER02/Q2_7_2.py
@@ -0,0 +1,6 @@
+rist = ["orange", "apple", "geape", "banana"]
+rist.sort()
+print(rist)
+
+rist.sort(reverse=True)
+print(rist)

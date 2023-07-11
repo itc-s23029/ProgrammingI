46269bb116f72181de3493ebb40a7857331165dc
yamaki koudai <s23029@std.it-college.ac.jp>
2023-07-11 07:12:01 +0000

    2.11 複数同時代入

---
+++ CHAPTER02/Q2_11_1.py
@@ -0,0 +1,5 @@
+a, b, c, d = 1, 2, 3, 4
+a, b, c, d = d, c, b, a
+a, b, c, d = b, d, a, c
+a, b, c, d = c, a, b, d
+print(c)
---
+++ CHAPTER02/Q2_11_2.py
@@ -0,0 +1,6 @@
+a = 1
+b = 2
+a, b = b, a + 4
+a, b = b, a + 1
+a, b = b, a + b
+print(a)
